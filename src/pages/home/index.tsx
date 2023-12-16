85b579996c7b0d15f10e45f371eea9ca93270923
nullsploit01 <harshaldharmik099@gmail.com>
2023-12-17 00:03:03 +0530

    add axios

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import Layout from '@/components/layout'
 const HomePage = () => {
   return (
     <Layout>
-      <div className="flex justify-center">Coming Soon!</div>
+      <div className="flex justify-center h-52">Coming Soon!</div>
     </Layout>
   )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,16 @@
 import { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
+import { axiosClient } from '@/config'
+
 const HomePage = lazy(() => import('@/pages/home'))
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />
+    element: <HomePage />,
+    loader: async () => {
+      return await axiosClient.get('/standings?league=39&season=2023')
+    }
   }
 ])
